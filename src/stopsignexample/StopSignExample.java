2d940ee179a2603664d94f765aa4709d1abacbb6
SKubjana <102598664+skubjana@users.noreply.github.com>
2026-02-25 14:18:13 +0200

    Final Submission

---
+++ src/stopsignexample/StopSignExample.java
@@ -0,0 +1,45 @@
+/*************************************************************************
+ * A simple java application that displays the 'STOP' and the 'GO' signs *
+ * using 'X' or 'Asterics' (*)                                           *
+ * ***********************************************************************/
+
+package stopsignexample;
+
+/**
+ * 
+ * @author ST1009246
+ */
+public class StopSignExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //The two figures will be displayed exactly as they are since the 
+        //alignment does not include any escape sequences.
+        
+                
+        
+        System.out.println("                -----------             \n ");
+        System.out.println("     ********                 XXXXXXXX        ");
+        System.out.println("   *          *             X          X      ");
+        System.out.println("  *  S T O P   *           X     GO     X     ");
+        System.out.println("   *          *             X          X      ");
+        System.out.println("     ********                 XXXXXXXX        ");
+        System.out.println("        **                       XX           ");
+        System.out.println("        **                       XX           ");
+        System.out.println("        **                       XX           ");
+        System.out.println("        **                       XX           ");
+        System.out.println("       ****                     XXXX          ");
+        System.out.println("    **********               XXXXXXXXXX       ");
+        System.out.println("|-----------------------------------------|");
+        System.out.println("|-----------------------------------------|");
+      
+    }
+    
+ /**************************************************************************
+  * Please note that this is not the only approach to the problem.         *
+  * Author: Sam                                                            *
+  *************************************************************************/
+}
